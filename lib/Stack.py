98538b6572d75b3599cc91f1d5eb5a7db6c72b28
Michael Muniu <muniu.dev@gmail.com>
2024-02-16 00:30:44 +0300

    Lab complete

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,42 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if self.size() != 0:
+            return False
+        else:
+            return True
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            value = self.items[-1]
+            del self.items[-1]
+            return value
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if self.size() == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        depth = 0
+        while depth != self.size():
+            if self.items[0 - (depth + 1)] == target:
+                return depth
+            else:
+                depth += 1
+        return -1
